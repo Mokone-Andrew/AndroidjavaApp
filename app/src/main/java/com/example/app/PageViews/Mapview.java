acdea4ab048ffd8f1e22f25433bf00acf11e8535
andrew <andrew.mokone@eoh.com>
2020-02-06 14:00:16 +0200

    first commit

---
+++ app/src/main/java/com/example/app/PageViews/Mapview.java
@@ -0,0 +1,16 @@
+package com.example.app.PageViews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.app.R;
+
+public class Mapview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mapview);
+    }
+}
---
+++ app/src/main/java/com/example/app/PageViews/Sign_Up.java
@@ -0,0 +1,16 @@
+package com.example.app.PageViews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.app.R;
+
+public class Sign_Up extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign__up);
+    }
+}
